277c0e8b2d9ef589c57bc9151155eed2406aef9c
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2022-11-28 17:48:10 -0300

    feat: poc threads

--- index.js
+++ index.js
@@ -1,27 +1,171 @@
 
-const { Builder, By, Key} = require('selenium-webdriver');
-
+const { Builder, By, Key } = require('selenium-webdriver');
+const { fork } = require('child_process')
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
 const port = 3000
-
+app.use(bodyParser.json());
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
 
 
-app.get('/test',async (request, response) => {
-try{
-    let links = await Scraping();
-    response.status(200).json(links)
-}catch(error){
-    console.log(error)
+//create uuid
+const uuid = require('uuid');
+
+
+class mutex {
+    constructor() {
+        this.locked = false
+    }
+
+    acquire() {
+        if (this.locked) {
+            return false;
+        }
+        else {
+            this.locked = true
+
+            return true
+        }
+    }
+
+    release() {
+        this.locked = false
+    }
+}
+
+
+
+let crawlerId = 0
+let mutexOcioso = new mutex()
+
+let child = null
+
+let lastStatus = 'ocioso'
+
+
+
+const createChild = () => {
+
+    //checkar se child ainda esta conectado
+    if (child != null && child.connected) {
+        console.log('child ainda vivo, matando')
+        child.disconnect()
+    }
+
+    child = fork('./mock_crawler.js')
+    console.log("______________ nova thread _____________")
+    console.log("PID do crawler recem criado: " + child.pid)
+
+
+    child.on('message', ([type, message]) => {
+
+        console.log("Crawler " + crawlerId)
+
+        console.log(message)
+
+
+        if (type == 'end') {
+            mutexOcioso.release()
+            console.log('Crawler ' + crawlerId + ' acabou com sucesso')
+        }
+        else if (type == 'status') {
+            lastStatus = message
+        }
+        else if (type == 'stop') {
+            lastStatus = message
+        }
+    })
+
+    child.on('error', (error) => {
+        console.log("Um erro fatal aconteceu, o que nao deveria rolar ja que tenho um try catch")
+        console.log(error)
+        mutexOcioso.release()
+    })
+
+}
+app.get('/health', (req, res) => {
+
+    if (mutexOcioso.locked) {
+        child.send(['status'])
+        res.send(`Crawler ${crawlerId} ocupado. Ultimo status: ${lastStatus}`)
+    }
+    else {
+        res.send(`Crawler ocioso`)
+    }
+
+})
+
+app.get('/stop', async (req, res) => {
+
+    if (!mutexOcioso.locked) {
+        res.send('Crawler ocioso, nao ha nada para parar')
+        return
+    }
+
+    //envia stop para filho
+    child.send(['stop'])
+
+    //indica que o crawler esta ocioso
+    mutexOcioso.release()
+    console.log("Crawler parado forçadamente com sucesso")
+
+    //criar outro processo ja que o anterior foi parado nao é necessario
+    res.send(`Crawler ${crawlerId} interrompido`)
+
+})
+
+app.post('/start', async (req, res) => {
+
+    const timeout = req.body.timeout
+    const percentage = req.body.percentage
+
+
+    const estaOcioso = mutexOcioso.acquire()
+
+    if (!estaOcioso) {
+        res.send(`Crawler ${crawlerId} ocupado`)
+        return
+    }
+    //criar nova thread
+    createChild()
+    crawlerId = uuid.v4()
+
+
+    parameters = {
+        timeout: timeout ?? 3000,
+        percentage: percentage ?? 0.5,
+        id: crawlerId
+    }
+    //criar processo se o ultimo acabou, nao deve acontecer
+    if (child == null || child.killed || child.exitCode != null) {
+        console.warn("Crawler morreu, criando novo processo")
+       
+    }
+
+    //enviar mensagem de inicio e parametros
+    child.send(["start", parameters])
+
+    //devolver uuid
+    res.send(`Crawler ${crawlerId} started`)
+
+})
+
+app.get('/test', async (request, response) => {
+    try {
+        let links = await Scraping();
+        response.status(200).json(links)
+    } catch (error) {
+        console.log(error)
     }
 })
 
-async function Scraping(){
+async function Scraping() {
     const driver = await new Builder().forBrowser('chrome').build()
     let pdfDetails = [];
     try {
@@ -32,13 +176,13 @@ async function Scraping(){
         elemento = await driver.findElement(By.className('mat-icon notranslate mat-tooltip-trigger icon cursor-pointer ml-5 material-icons mat-icon-no-color'));
         await driver.sleep(2000)
         await elemento.click();
-        
+
 
         // SELECIONA MONOCRATICAS
         await driver.sleep(2000)
         elemento = await driver.findElement(By.xpath('/html/body/app-root/app-home/main/search/div/search-input/div/div/div/div/div[2]/div/div[1]/div/mat-radio-group/span[4]/mat-radio-button/label/div[1]/div[2]'))
         elemento.click()
-        
+
 
         // DESABILITA BUSCA ENTRE ASPAS
         await driver.sleep(2000)
@@ -67,25 +211,25 @@ async function Scraping(){
         driver.sleep(2000)
 
         var pdfs = await driver.findElements(By.className('mat-tooltip-trigger ng-star-inserted'))
-       
+
         for (const pdf of pdfs) {
             let link = await pdf.getAttribute('href')
             linksDetails.push({
                 link: link ?? 'Não tem pdf',
             });
-            
-        }
-         
-       
-      } catch (error) {
-          console.log(error);
-          return false
-      } finally {
-        await driver.quit();
-      }
 
-      return pdfDetails
-      
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        return false
+    } finally {
+        await driver.quit();
+    }
+
+    return pdfDetails
+
 }
 
 
---
+++ mock_crawler.js
@@ -0,0 +1,75 @@
+// Creates a mock crawler that can be used for testing
+
+const { signal } = require("nodemon/lib/config/defaults")
+const { resource } = require("selenium-webdriver/http")
+
+
+process.on('message', ( message) => {
+    const type = message[0]
+
+    if (type === 'start') {
+        const { timeout, percentage, callback } = message[1]
+
+        Crawler(timeout, percentage, callback)
+    }
+    else if (type == 'status'){
+        //mandar aqui pagina atual, etc
+        process.send(['status', 'ok, pagina x'])
+    }
+    else if (type === 'stop') {
+        process.send(['stop', 'parado na pagina x'])
+        console.log("Crawler: Parando crawler..")
+       
+        process.exit()
+       
+
+    }
+
+
+})
+
+
+
+async function Crawler(timeout = 2, percentage = 0.4, callback) {
+    
+
+        const result = {
+            success: false,
+            error: null,
+            stacktrace: null,
+            executionTime: null,
+            lastPage: null
+        }
+
+        start = new Date().getTime()
+        await setTimeout(() => {
+            try {
+            const randInt = Math.random()
+            if (randInt < percentage ) {
+                throw new Error("Erro aleatorio")
+            }
+            result.success = true
+            result.executionTime = new Date().getTime() - start
+            result.lastPage = 1
+            
+            console.log("Crawler: Fim do crawler com sucesso")
+            
+        }
+        catch (error) {
+            result.error = error.message
+            result.stacktrace = error.stack
+            console.log("Crawler: Erro no crawler com pid " + process.pid)
+        }
+        finally{
+            if (process.connected){
+                process.send(['end', result])
+                process.disconnect()
+            }
+        }
+
+        }
+            , timeout)
+    
+}
+
+module.exports = Crawler
