076f0e7f6a53e093b0d8c578cfa676b412209a53
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-01-12 16:10:57 -0300

    feat: criar objects

---
+++ objects/TJAC/acordao.js
@@ -0,0 +1,23 @@
+let Acordao = {
+    tipo_publicacao : 'DECISAO_MONOCRATICA',
+    cod_cnj_tj: '100',
+    processo : "",
+    classe : "",
+    assunto : "",
+    relator : "",
+    orgao_julgador: "",
+    data_julgamento : "",
+    data_publicacao : "",
+    //esta na parte ementa sem formatacao
+    ementa_full: "",
+    //deve ser limpa sem tag html
+    ementa: "",
+    linha_citacao: "",
+    numero_unico_cnj : "",
+    numeros_origem : "",
+    url_pdf: "",
+    tipo_jurisdicao : 'INSTANCIA',
+    comarca: "",
+}
+
+module.exports = Acordao
---
+++ objects/TJAC/monocratica.js
@@ -0,0 +1,19 @@
+let Monocratica = {
+    tipo_publicacao : 'DECISAO_MONOCRATICA',
+    cod_cnj_tj: '100',
+    processo : "",
+    classe : "",
+    assunto : "",
+    relator : "",
+    orgao_julgador: "",
+    data_julgamento : "",
+    data_publicacao : "",
+    linha_citacao: "",
+    numero_unico_cnj : "",
+    numeros_origem : "",
+    url_pdf: "",
+    tipo_jurisdicao : 'INSTANCIA',
+    comarca: "",
+}
+
+module.exports = Monocratica
